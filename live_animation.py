7cf3f02b670d241b41381e7d194cdd63cb7321ce
Cory Hargus <cory.hargus@gmail.com>
2019-04-03 01:26:07 -0700

    Working version with real fish!

--- live_animation.py
+++ live_animation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Use Matplotlib to generate live animation of cat dynamics.
 
@@ -11,6 +12,10 @@ from matplotlib.offsetbox import (TextArea, DrawingArea, OffsetImage,
                                   AnnotationBbox, AnchoredOffsetbox)
 from matplotlib import animation
 import cat_dynamics
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 np.random.seed(0)
 plt.rcParams['toolbar'] = 'None'
 
@@ -82,8 +87,10 @@ def initialize_fig(outdir, ptraj, cat_ptraj, ttraj, cat_ttraj, L,
 
 
 # Define simulation parmeters and initialize dynamics:
-n = 300
-ncat = 7
+names = """alois jason curtis sarah cory andrew na-young soo-yeon mitch
+        """.split()
+n = 20
+ncat = len(names)
 rho = 4.
 L = np.sqrt(n / rho)
 eta = 1.2
@@ -93,40 +100,31 @@ cat_vel = 0.02
 cat_pull = -0.3
 rcscale = .05
 nframes = 4000
-nlog = 10
+nlog = 1
 pos, thetas, L = cat_dynamics.initialize(n, rho)
 cat_pos = np.random.uniform(L * .1, L * .9, size=(ncat, 2))
 cat_thetas = np.random.uniform(-np.pi, np.pi, size=ncat)
+mod = False
 rcut = rcscale * L
-
-# Begin iterating dynamics calculations:
-# for i in range(nframes):
-#     if i % nlog == 0:
-#         ptraj[i / nlog] = pos
-#         ttraj[i / nlog] = thetas
-#         cat_ptraj[i / nlog] = cat_pos
-#         cat_ttraj[i / nlog] = cat_thetas
-#     pos[:], thetas[:] = timestep(pos, thetas, rcut, eta, vel, L, mod,
-#                                  cat_pos, cat_pull)
-#     cat_pos[:], cat_thetas[:] = timestep(cat_pos, cat_thetas, rcut,
-#                                          cat_eta, cat_vel, L, mod)
 
 
 def data_gen():
     while True:
         for _ in range(nlog):
+            pos[:], thetas[:] = cat_dynamics.timestep(
+                pos, thetas, rcut, eta, vel, L, mod, cat_pos, cat_pull)
             cat_pos[:], cat_thetas[:] = cat_dynamics.timestep(
                 cat_pos, cat_thetas, rcut, cat_eta, cat_vel, L)
-        yield cat_pos, cat_thetas
+        yield pos, thetas, cat_pos, cat_thetas
 
 
 # Initialize matplotlib figure
 fig, ax = plt.subplots(facecolor='black', figsize=(8, 8))
-# fig = plt.figure(figsize=(8,8), facecolor='black')
-# ax = fig.add_subplot(111)
 background = img.imread('f/bg2.png')
 # background = background[::-1, :, :]  # Invert image (else it's upside down)
 ax.imshow(background, alpha=0.9, zorder=0)
+
+# Add cats:
 
 ibb = [OffsetImage(img.imread('icons/{}b.png'.format(i)), zoom=1.3) for
        i in range(ncat)]
@@ -137,33 +135,67 @@ abbox = [AnnotationBbox(ib, [0, 0], xycoords='data', frameon=False) for
 for ab in abbox:
     ax.add_artist(ab)
 
-scalefactor = ax.get_xlim()[1]
-# print scalefactor, L
-# imb = img.imread('icons/0b.png')
-# imageboxb = OffsetImage(imb, zoom=0.8)
-# imf = img.imread('icons/0f.png')
-# imageboxf = OffsetImage(imf, zoom=0.8)
-# # imagebox.image.axes = ax
-# # Initialize in backwards facing in middle:
-# cat_pos = [.5, .5]
-# ab = AnnotationBbox(imageboxb, cat_pos, xycoords='data',
-#                     frameon=False)
-# # Then reset to forwards facing in corner:
-# ab.offsetbox = imageboxf
-# ab.xybox = [.9, .9]
-# ax.add_artist(ab)
+# Add labels:
+labels = []
+for i in range(ncat):
+    bbox = {'fc': '0.8', 'pad': 0, 'facecolor': 'white',
+            'alpha': 0.8, 'pad': 0}
+    props = {'ha': 'center', 'va': 'bottom', 'bbox': bbox, 'color': 'k',
+             'fontsize': 24, 'family': 'monospace', 'weight': 'bold',
+             'zorder': 100}
+    labels.append(ax.text(0, 0, names[i], props))
 
-# Specify animation settings:
+# Add fish:
+# fishstr = r"""
+#       /`·.¸
+#      /¸...¸`:·
+#  ¸.·´  ¸   `·.¸.·´)
+# : * ):´;      ¸  {
+#  `·.¸ `·  ¸.·´`·¸)
+#      `´´¸.·´
+# """
+# fishstr = r"""😃"""
+# bbox = {'fc': '0.8', 'pad': 0}
+# props = {'ha': 'center', 'va': 'center', 'bbox': bbox, 'color': 'b',
+#          'fontsize': 20, 'family': 'monospace'}
+# fish = [ax.text(0, 0, fishstr, props) for i in range(n)]
+angles = range(0, 360, 30)
+ima = [OffsetImage(img.imread('icons/fish/a{}.png'.format(a)), zoom=1.3) for
+       a in angles]
+fishbox = [AnnotationBbox(ima[0], [0, 0], xycoords='data', frameon=False) for
+           _ in range(n)]
+for ab in fishbox:
+    ab.set_zorder(1)
+    ax.add_artist(ab)
+
+scalefactor = ax.get_xlim()[1]
+
+# Specify animation settings
 
 
 def update(data):
-    cat_pos, cat_thetas = data
+    pos, thetas, cat_pos, cat_thetas = data
+    # Update cats:
     for i in range(len(abbox)):
         abbox[i].xybox = cat_pos[i] * scalefactor / L
         if np.sin(cat_thetas[i]) > 0:
             abbox[i].offsetbox = ibf[i]  # Face forwards
         else:
             abbox[i].offsetbox = ibb[i]  # Face backwards
+
+    # Update labels:
+    for i in range(ncat):
+        shift = np.array([0, .1])
+        labels[i].set_position((cat_pos[i] + shift) * scalefactor / L)
+
+    # Update fish:
+    for i in range(n):
+        fishbox[i].xybox = pos[i] * scalefactor / L
+        theta360 = (thetas[i] + np.pi) * (180. / np.pi)
+        angleid = (int(theta360 + 15) % 360) / 30
+        print angleid
+        fishbox[i].offsetbox = ima[angleid]  # Orientation
+
     return abbox,
 
 
